5e578c5b614094badb3b0c5a84e5acc222f44d67
Gimbrint <100514371+Gimbrint@users.noreply.github.com>
2022-08-19 17:56:40 +0300

    Added a randomUtility file
    Added a file which can pick a random item from a list

---
+++ randomUtility.py
@@ -0,0 +1,40 @@
+import logging
+import random
+
+def get_random_item(items : list, probabilities : list=None):
+	# No probabilities given, so make it evenly distributed
+	if probabilities == None:
+		probabilities = get_default_probabilities(items=items)
+
+	# Number of items and probabilities
+	itemLength = items.__len__()
+	probLength = probabilities.__len__()
+
+	# There must be the same items of probabilities as the number of items
+	if probLength < itemLength or probLength > itemLength:
+		logging.error(" Number of probabilities not the same as number of items")
+		return None
+
+	# Generate a random number
+	randomValue = random.random()
+
+	# Current weight (all previous weights + current probability)
+	currentWeight = 0
+
+	# Go through each items
+	for i in range(0, itemLength, 1):
+		currentWeight += probabilities[i]
+
+		if randomValue < currentWeight:
+			# We found our item!
+			return items[i]
+
+# Generates equal probabilities if none are specified
+def get_default_probabilities(items : list) -> list:
+	length = items.__len__()
+	probabilities = [None] * length # Why this? Why not just use list(<length>)
+
+	for i in range(0, length, 1):
+		probabilities[i] = 1 / length
+
+	return probabilities
